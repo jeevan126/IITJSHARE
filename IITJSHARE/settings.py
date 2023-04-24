028eb14056ff880752dd228c9420e303fb290838
Jeevan Singh <singh.126@iitj.ac.in>
2023-04-24 12:46:36 +0530

    change

--- IITJSHARE/settings.py
+++ IITJSHARE/settings.py
@@ -172,7 +172,7 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+EMAIL_PORT = (os.environ.get('EMAIL_PORT'))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
