405f82ec5c06bff363e208ee83384e45e3b5eed1
Jeevan Singh <singh.126@iitj.ac.in>
2023-04-25 06:24:15 +0530

    crpfnew

--- IITJSHARE/settings.py
+++ IITJSHARE/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     
 }
 CSRF_TRUSTED_ORIGINS = [
-    'https://web-production-fb83.up.railway.app/'
+    'web-production-fb83.up.railway.app'
 ]
 # "ENGINE": "django.db.backends.mysql",
         # 'NAME': os.getenv('MYSQLDATABASE'),
